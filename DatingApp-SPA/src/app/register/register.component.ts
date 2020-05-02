d228a7c1f805147c5a47e1ce02399a13f3b34c4f
ilkeryanizca <ilker.yanizca@yahoo.com.tr>
2020-05-02 14:24:23 +0300

    Section 4 Complated.

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  // Parent dan child a gelen veri.
+  // @Input() valuesFromHome: any;
+
+  // Child dan parent e gönderilen veri.
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registiration success.');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
