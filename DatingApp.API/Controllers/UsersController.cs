0f10660a55e5f0a3db475cc7ecf28cca4da03313
ilkeryanizca <ilker.yanizca@yahoo.com.tr>
2020-05-24 02:24:03 +0300

    Section 15 : Like Function - Complated in SPA.

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -85,7 +85,7 @@ namespace DatingApp.API.Controllers
             var like = await _repo.GetLike(id, recipientId);
 
             if (like != null)
-                return BadRequest("You already like this user.");
+                return BadRequest("You already liked this user");
 
             if (await _repo.GetUser(recipientId) == null)
                 return NotFound();
